45b3e8d80015740467ac03c0a0835dad66bb5eb0
ngmeldin-adam <101349423+ngmeldin-adam@users.noreply.github.com>
2025-11-01 10:04:31 +0300

    Update db.js

--- server/lib/db.js
+++ server/lib/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async()=>{
     try{
         
-        await mongoose.connect(process.env.MONGO_URI,
+        await mongoose.connect(process.env.MONGO,
             {
                 useNewUrlParser:true,
                 useUnifiedTopology:true
